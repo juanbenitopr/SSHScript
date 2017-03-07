5d5446ac0cc6ce13bf15eddcf2a5bc83e9e92779
Juan Benito <juanbenito.pr@gmail.com>
2017-03-07 14:19:08 +0100

    fix problem using threading

--- ssh_script_file.py
+++ ssh_script_file.py
@@ -17,7 +17,7 @@ def worker_ssh():
     ssh_instance.login(host, username=username, password=password)
     ssh_instance.sendline(command)
     ssh_instance.prompt()
-    print ssh_instance.before
+    print (ssh_instance.before.decode('utf-8'))
     ssh_instance.logout()
 
 
@@ -25,9 +25,9 @@ try:
     threads_host = []
     for host in hosts_list:
         t = Thread(target=worker_ssh)
-        t.start()
-        threads_host.append(t)
+        threads_host.append(t.start())
 
-except pxssh.ExceptionPxssh, e:
-    print "pxssh failed on login"
-    print str(e)
+
+except pxssh.ExceptionPxssh as e:
+    print ("pxssh failed on login")
+    print (str(e))
