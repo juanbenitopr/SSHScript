f58681538f9102db95133a1f1519d5c54f610c64
JuanB <Juan Benito>
2017-03-07 11:52:26 +0100

    Initial commit

---
+++ ssh_script_file.py
@@ -0,0 +1,29 @@
+import sys
+from threading import Thread
+
+from pexpect import pxssh
+
+
+hosts = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+command = sys.argv[4]
+
+hosts_list = [host for host in hosts.split(" ")]
+
+
+def worker_ssh():
+    ssh_instance = pxssh.pxssh()
+    ssh_instance.login(host, username=username, password=password)
+    ssh_instance.sendline(command)
+    ssh_instance.prompt()
+    print ssh_instance.before
+    ssh_instance.logout()
+
+
+try:
+    threads_host = [Thread(target=worker_ssh()).start() for host in hosts_list]
+
+except pxssh.ExceptionPxssh, e:
+    print "pxssh failed on login"
+    print str(e)
