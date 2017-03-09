8391c9d617f98b00b95e58eb4a7dbb45116f2c74
Juan Benito <juanbenito.pr@gmail.com>
2017-03-09 10:18:20 +0100

    Python 3.5 and improve the structure

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # SSHScript
 It is a simple script where you can specify your hosts, username and pass and sending commands to different hosts.
 
-1.  First argument, hosts (separated by space), I am working for reading the hosts from a txt, and you can specify some specific format for usename and password
-2.  Second argument username
-3.  Third argument password
-4.  Fourth argument command to execute
+All ssh conections are executed in different threads then you don't bother the rest of connection. I am working for the migration from python2.7 using the threading package to python 3.5 using Asyncio. This will be useful if you want to connect to a lot of machines
 
-
-All this ssh conections are executed in different threads then you don't bother the rest of conection. I am working for the migration from python2.7 using the threading package to python 3.5 using Asyncio. This will be useful if you want to connect to a lot of machines 
-
+The script has a few options from specify hosts in txt to do all the options from command line you just have to write the command -h for view the help
 
 example:
-  ssh_script_file.py "host1 host2" username password "command"
+  ssh_script_file.py -h
--- ssh_script_file.py
+++ ssh_script_file.py
@@ -1,33 +1,76 @@
+import argparse
 import sys
 from threading import Thread
 
 from pexpect import pxssh
 
 
-hosts = sys.argv[1]
-username = sys.argv[2]
-password = sys.argv[3]
-command = sys.argv[4]
-
-hosts_list = [host for host in hosts.split(" ")]
+# hosts = sys.argv[1]
+# username = sys.argv[2]
+# password = sys.argv[3]
+# command = sys.argv[4]
 
 
-def worker_ssh():
-    ssh_instance = pxssh.pxssh()
-    ssh_instance.login(host, username=username, password=password)
-    ssh_instance.sendline(command)
-    ssh_instance.prompt()
-    print (ssh_instance.before.decode('utf-8'))
-    ssh_instance.logout()
+class SSHConnectionHosts():
+    def __init__(self):
+        self.hosts = []
+        self.command = ""
+        self.password = ""
+        self.username = ""
 
 
-try:
-    threads_host = []
-    for host in hosts_list:
-        t = Thread(target=worker_ssh)
-        threads_host.append(t.start())
+    def add_hosts(self,hosts):
+        if hosts: self.hosts = [ host for host in hosts.split(" ") ]
+
+    def add_hosts_text(self,path):
+        if path:
+            hosts_in_file = open(path,'r')
+            for host in hosts_in_file:
+                self.hosts.append(host.replace("\n",""))
 
 
-except pxssh.ExceptionPxssh as e:
-    print ("pxssh failed on login")
-    print (str(e))
+    def set_command (self,command):
+        self.command = command
+
+    def set_password(self,password):
+        self.password  =password
+
+    def set_username(self,username):
+        self.username = username
+
+
+    def worker_ssh(self,host):
+        ssh_instance = pxssh.pxssh()
+        ssh_instance.login(host, username=self.username, password=self.password)
+        ssh_instance.sendline(self.command)
+        ssh_instance.prompt()
+        print (ssh_instance.before.decode('utf-8'))
+        ssh_instance.logout()
+
+    def run_ssh(self):
+        try:
+            threads_host = []
+            for host in self.hosts:
+                t = Thread(target=self.worker_ssh, kwargs={'host':host})
+                threads_host.append(t.start())
+        except pxssh.ExceptionPxssh as e:
+            print ("pxssh failed on login")
+            print (str(e))
+
+sshconnection = SSHConnectionHosts()
+parser = argparse.ArgumentParser(prog='ssh_script_file',description='Sending commands to many hosts')
+
+parser.add_argument('-hosts', help="Specify name of the hosts by command line")
+parser.add_argument('-H', help = "Specify file where you store your host names")
+parser.add_argument('-u', help ="Specify your username for hosts")
+parser.add_argument('-p', help="Specify the password for the entry to the hosts")
+parser.add_argument('-c', help="The command to use in the different servers")
+
+
+args = parser.parse_args()
+sshconnection.add_hosts(args.hosts)
+sshconnection.add_hosts_text(args.H)
+sshconnection.set_username(args.u)
+sshconnection.set_password(args.p)
+sshconnection.set_command(args.c)
+sshconnection.run_ssh()
