cb59ed356ada0726c97c9af848064224d4c80103
Juan Benito <juanbenito.pr@gmail.com>
2017-03-10 09:35:25 +0100

    Python 3.5 and improve the structure

--- ssh_script_file.py
+++ ssh_script_file.py
@@ -14,35 +14,49 @@ from pexpect import pxssh
 class SSHConnectionHosts():
     def __init__(self):
         self.hosts = []
+        self.hosts_and_pass = []
         self.command = ""
         self.password = ""
         self.username = ""
 
 
     def add_hosts(self,hosts):
-        if hosts: self.hosts = [ host for host in hosts.split(" ") ]
+        if hosts: self.hosts_and_pass = [ {'host':host} for host in hosts.split(" ") ]
 
     def add_hosts_text(self,path):
         if path:
             hosts_in_file = open(path,'r')
             for host in hosts_in_file:
-                self.hosts.append(host.replace("\n",""))
+                self.hosts.append({'host':host.replace("\n","")})
+
+    def add_hosts_and_pass_text(self,path):
+        if path:
+            hosts_in_file = open(path,'r')
+            for host_and_pass in hosts_in_file:
+                host_and_pass = host_and_pass.replace("\n", "").split(":")
+                self.hosts_and_pass.append({'host':host_and_pass[0],'password':host_and_pass[1]})
+
 
 
     def set_command (self,command):
-        self.command = command
+        for index in range(len(self.hosts_and_pass)):
+            self.hosts_and_pass[index]['command'] = command
+
 
     def set_password(self,password):
-        self.password  =password
+        for index in range(len(self.hosts_and_pass)):
+            self.hosts_and_pass[index]['password'] = password
 
     def set_username(self,username):
-        self.username = username
+        for index in range(len(self.hosts_and_pass)):
+            self.hosts_and_pass[index]['username'] = username
 
 
-    def worker_ssh(self,host):
+
+    def worker_ssh(self,host,password,command,username):
         ssh_instance = pxssh.pxssh()
-        ssh_instance.login(host, username=self.username, password=self.password)
-        ssh_instance.sendline(self.command)
+        ssh_instance.login(host, username=username, password=password)
+        ssh_instance.sendline(command)
         ssh_instance.prompt()
         print (ssh_instance.before.decode('utf-8'))
         ssh_instance.logout()
@@ -50,8 +64,8 @@ class SSHConnectionHosts():
     def run_ssh(self):
         try:
             threads_host = []
-            for host in self.hosts:
-                t = Thread(target=self.worker_ssh, kwargs={'host':host})
+            for host in self.hosts_and_pass:
+                t = Thread(target=self.worker_ssh, kwargs=host)
                 threads_host.append(t.start())
         except pxssh.ExceptionPxssh as e:
             print ("pxssh failed on login")
@@ -62,6 +76,9 @@ parser = argparse.ArgumentParser(prog='ssh_script_file',description='Sending com
 
 parser.add_argument('-hosts', help="Specify name of the hosts by command line")
 parser.add_argument('-H', help = "Specify file where you store your host names")
+parser.add_argument('-Hp', help = "Specify file where you store your host names and password example  host:password")
+parser.add_argument('-Hu', help = "Specify file where you store your host names and password example  host:username")
+parser.add_argument('-A', help = "Specify file where you store all your datas example  host:username:password:command")
 parser.add_argument('-u', help ="Specify your username for hosts")
 parser.add_argument('-p', help="Specify the password for the entry to the hosts")
 parser.add_argument('-c', help="The command to use in the different servers")
@@ -70,6 +87,7 @@ parser.add_argument('-c', help="The command to use in the different servers")
 args = parser.parse_args()
 sshconnection.add_hosts(args.hosts)
 sshconnection.add_hosts_text(args.H)
+sshconnection.add_hosts_and_pass_text(args.Hp)
 sshconnection.set_username(args.u)
 sshconnection.set_password(args.p)
 sshconnection.set_command(args.c)
